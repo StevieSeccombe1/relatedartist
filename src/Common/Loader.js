37718c2d8add56a0a185a36574e46dfea4986ce2
sseccomb <stevie.l.seccombe@aexp.com>
2019-03-03 20:05:50 +0000

    changed loader

--- src/Common/Loader.js
+++ src/Common/Loader.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import { Animated } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome5";
 
 const FullWidthWrapper = styled.View`
   align-items: center;
@@ -12,14 +13,12 @@ const Wrapper = styled(Animated.View)`
   width: 46px;
 `;
 
-const Center = styled.View`
+const Note = styled.View`
   height: 24px;
   width: 24px;
-  border-radius: 12px;
-  background: white;
 `;
 
-const OrbitLine = styled.View`
+const Circle = styled.View`
   height: 46px;
   width: 46px;
   border-radius: 23px;
@@ -29,40 +28,31 @@ const OrbitLine = styled.View`
   border-color: white;
 `;
 
-const Moon = styled.View`
-  height: 14px;
-  width: 14px;
-  border-radius: 7px;
-  background: white;
-  position: absolute;
-  left: 34px;
-  top: 0;
-`;
-
 export default class extends Component {
   state = {
-    moonAnim: new Animated.Value(0)
+    anim: new Animated.Value(0)
   };
 
   componentDidMount() {
     Animated.loop(
-      Animated.timing(this.state.moonAnim, { toValue: 1, duration: 1000 })
+      Animated.timing(this.state.anim, { toValue: 1, duration: 1000 })
     ).start();
   }
 
   render() {
-    const moonRotation = this.state.moonAnim.interpolate({
+    const rotation = this.state.anim.interpolate({
       inputRange: [0, 1],
       outputRange: ["0deg", "360deg"]
     });
 
     return (
       <FullWidthWrapper>
-        <Wrapper style={{ transform: [{ rotate: moonRotation }] }}>
-          <OrbitLine>
-            <Center />
-          </OrbitLine>
-          <Moon />
+        <Wrapper style={{ transform: [{ rotate: rotation }] }}>
+          <Circle>
+            <Note>
+              <Icon name="music" size={20} color="white" />
+            </Note>
+          </Circle>
         </Wrapper>
       </FullWidthWrapper>
     );
